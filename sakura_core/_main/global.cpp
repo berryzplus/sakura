89cf8684f386494c677036b87dc816a7c4d27b10
berryplus <berryzplus@gmail.com>
2019-03-03 13:43:17 +0900

    シンボル定義を移動する

--- sakura_core/_main/global.cpp
+++ sakura_core/_main/global.cpp
@@ -30,3 +30,34 @@ HINSTANCE G_AppInstance()
 {
 	return CProcess::getInstance()->GetProcessInstance();
 }
+
+// アプリ名。2007.09.21 kobake 整理
+#ifdef _UNICODE
+#define _APP_NAME_(TYPE) TYPE("sakura")
+#else
+#define _APP_NAME_(TYPE) TYPE("sakura")
+#endif
+
+#ifdef _DEBUG
+#define _APP_NAME_2_(TYPE) TYPE("(デバッグ版)")
+#else
+#define _APP_NAME_2_(TYPE) TYPE("")
+#endif
+
+#ifdef ALPHA_VERSION
+#define _APP_NAME_3_(TYPE) TYPE("(Alpha Version)")
+#else
+#define _APP_NAME_3_(TYPE) TYPE("")
+#endif
+
+#ifdef APPVEYOR_DEV_VERSION
+#define _APP_NAME_DEV_(TYPE) TYPE("(dev Version)")
+#else
+#define _APP_NAME_DEV_(TYPE) TYPE("")
+#endif
+
+#define _GSTR_APPNAME_(TYPE)  _APP_NAME_(TYPE) _APP_NAME_2_(TYPE) _APP_NAME_DEV_(TYPE) _APP_NAME_3_(TYPE)
+
+const TCHAR g_szGStrAppName[]  = (_GSTR_APPNAME_(_T)   ); // この変数を直接参照せずに GSTR_APPNAME を使うこと
+const CHAR  g_szGStrAppNameA[] = (_GSTR_APPNAME_(ATEXT)); // この変数を直接参照せずに GSTR_APPNAME_A を使うこと
+const WCHAR g_szGStrAppNameW[] = (_GSTR_APPNAME_(LTEXT)); // この変数を直接参照せずに GSTR_APPNAME_W を使うこと
