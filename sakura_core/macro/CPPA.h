8fb7267ba4bd44c674a6091ac4d3bb37e43d42f8
berryzplus <berryzplus@gmail.com>
2020-12-03 03:01:16 +0900

    auto_sprintf対策(必要のない呼出を置換)

--- sakura_core/macro/CPPA.h
+++ sakura_core/macro/CPPA.h
@@ -66,7 +66,7 @@ public:
 
 	const char* GetVersion(){		//!< DLLのバージョン情報を取得。m_szMsgを壊す
 		if ( IsAvailable() ){
-			auto_sprintf(m_szMsg, "PPA.DLL Version %d.%d", m_fnGetVersion() / 100, m_fnGetVersion() % 100);
+			::sprintf_s(m_szMsg, "PPA.DLL Version %d.%d", m_fnGetVersion() / 100, m_fnGetVersion() % 100);
 			return m_szMsg;
 		}
 		return "";
