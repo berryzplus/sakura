f826d9052d6852d0b7c9d43a5877fa864b106986
berryzplus <berryzplus@gmail.com>
2021-02-01 00:20:45 +0900

    CSakuraEnvironment,::ExpandParameterのテスト追加

--- tests/unittests/test-csakuraenvironment.cpp
+++ tests/unittests/test-csakuraenvironment.cpp
@@ -32,6 +32,7 @@
 #include <Windows.h>
 #include <Shlwapi.h>
 
+#include "config/app_constants.h"
 #include "config/maxdata.h"
 #include "basis/primitive.h"
 #include "debug/Debug2.h"
@@ -41,6 +42,26 @@
 #include "_main/CCommandLine.h"
 #include "_main/CControlProcess.h"
 #include "util/file.h"
+
+/*!
+ * @brief $記号の取得
+ */
+TEST(CSakuraEnvironment, ExpandParameter_GetDollar)
+{
+	SFilePath szBuf;
+	CSakuraEnvironment::ExpandParameter(L"$$", szBuf, _countof2(szBuf));
+	ASSERT_STREQ(L"$", szBuf.c_str());
+}
+
+/*!
+ * @brief アプリ名の取得
+ */
+TEST(CSakuraEnvironment, ExpandParameter_GetAppName)
+{
+	SFilePath szBuf;
+	CSakuraEnvironment::ExpandParameter(L"$A", szBuf, _countof2(szBuf));
+	ASSERT_STREQ(GSTR_APPNAME_W, szBuf.c_str());
+}
 
 /*!
  * @brief exeファイルパスの取得
@@ -87,6 +108,46 @@ TEST(CSakuraEnvironment, ExpandParameter_IniFileNameNamedProfile)
 	SFilePath szIniFile;
 	CSakuraEnvironment::ExpandParameter(L"$I", szIniFile, _countof2(szIniFile));
 	ASSERT_STREQ(GetIniFileName().c_str(), szIniFile.c_str());
+
+	// コマンドラインのグローバル変数を元に戻す
+	pCommandLine->ParseCommandLine(L"", false);
+}
+
+/*!
+ * @brief プロファイル名の取得
+ */
+TEST(CSakuraEnvironment, ExpandParameter_GetProfileNameNoProfile)
+{
+	// コマンドラインのグローバル変数をセットする
+	auto pCommandLine = CCommandLine::getInstance();
+	pCommandLine->ParseCommandLine(LR"(-PROF="")", false);
+
+	// プロセスのインスタンスを用意する
+	CControlProcess dummy(nullptr, LR"(-PROF="")");
+
+	SFilePath szBuf;
+	CSakuraEnvironment::ExpandParameter(L"$<profile>", szBuf, _countof2(szBuf));
+	ASSERT_STREQ(L"", szBuf.c_str());
+
+	// コマンドラインのグローバル変数を元に戻す
+	pCommandLine->ParseCommandLine(L"", false);
+}
+
+/*!
+ * @brief プロファイル名の取得
+ */
+TEST(CSakuraEnvironment, ExpandParameter_GetProfileNameNamedProfile)
+{
+	// コマンドラインのグローバル変数をセットする
+	auto pCommandLine = CCommandLine::getInstance();
+	pCommandLine->ParseCommandLine(LR"(-PROF="profile1")", false);
+
+	// プロセスのインスタンスを用意する
+	CControlProcess dummy(nullptr, LR"(-PROF="profile1")");
+
+	SFilePath szBuf;
+	CSakuraEnvironment::ExpandParameter(L"$<profile>", szBuf, _countof2(szBuf));
+	ASSERT_STREQ(L"profile1", szBuf.c_str());
 
 	// コマンドラインのグローバル変数を元に戻す
 	pCommandLine->ParseCommandLine(L"", false);
